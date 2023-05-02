20681d5a67bca72190faed3469849876c79081e4
josiah <felicityspyder@protonmail.comm>
2023-05-01 20:40:18 -0400

    revising playRound function

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let myArray = ['Rock', 'Paper', 'Scissors'];
 
+
 function getComputerChoice(myArray) {
     let selectRandom = Math.floor(Math.random() * myArray.length);
     return myArray[selectRandom];
@@ -8,25 +9,27 @@ function getComputerChoice(myArray) {
  function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
         return('You Win! Rock beats scissors.');
-    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-        return('You Lose! Rock beats Paper'); 
-    } else {
-        return('It\'s a tie! You both selected Rock. Try again.');
     } if (playerSelection === 'Paper' && computerSelection === 'Rock') {
         return('You Win! Paper beats Rock.');
-    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-        return('You lose! Scissors beats Paper.');
-    } else { 
-        return('It\'s a tie! You both selected Paper. Try again.')
     } if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
         return('You Win! Scissors beats Paper.');
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return('You Lose! Paper beats Rock'); 
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return('You lose! Scissors beats Paper.');
     } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
         return('You lose! Rock beats Scissors'); 
     } else {
-        return('It\'s a tie! You both selected Scissors. Try again.')
-    }   
+        return('It\'s a tie! Try again.');
+    }
+ }
+
+const playerSelection = prompt('Choose Rock, Paper, or Scissors') 
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-const playerSelection = "rock";
+
 const computerSelection = getComputerChoice(myArray);
 console.log(playRound(playerSelection, computerSelection));
