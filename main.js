619c851169fd15a4298b957bcf209f65b08582c9
josiah <felicityspyder@protonmail.comm>
2023-05-02 14:00:13 -0400

    ChatGPT aid with game(); function

--- main.js
+++ main.js
@@ -1,35 +1,59 @@
-let myArray = ['Rock', 'Paper', 'Scissors'];
+let myArray = ['rock', 'paper', 'scissors'];
 
 
 function getComputerChoice(myArray) {
     let selectRandom = Math.floor(Math.random() * myArray.length);
     return myArray[selectRandom];
  } 
- 
- function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-        return('You Win! Rock beats scissors.');
-    } if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-        return('You Win! Paper beats Rock.');
-    } if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
-        return('You Win! Scissors beats Paper.');
-    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-        return('You Lose! Paper beats Rock'); 
-    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-        return('You lose! Scissors beats Paper.');
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
-        return('You lose! Rock beats Scissors'); 
+
+function playRound(playerSelection, computerSelection) { //syntax burrowed from TOP example
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return alert('You win!' + ' Rock beats scissors.');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return('You win!' + ' Paper beats rock.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return('You win!' + ' Scissors beats paper.');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return('You lose!' + ' Paper beats rock'); 
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return('You lose!' + ' Scissors beats paper.');
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return('You lose!' + ' Rock beats scissors'); 
     } else {
-        return('It\'s a tie! Try again.');
+        return('It\'s a tie! Play again.')
     }
- }
-
-const playerSelection = prompt('Choose Rock, Paper, or Scissors') 
-
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
+function game() { //burrowed from ChatGPT 
+    let playerScore = 0;
+    let computerScore = 0;
 
-const computerSelection = getComputerChoice(myArray);
-console.log(playRound(playerSelection, computerSelection));
+    for (let i = 1; i <= 5; i++) {
+        const playerSelection = prompt(`Round ${i}: Choose, rock, paper or scissors`).toLowerCase();
+        const computerSelection = getComputerChoice(myArray);
+        const roundResult = playRound(playerSelection, computerSelection);
+
+        console.log(roundResult);
+
+        if (roundResult.includes('You win!')) {
+            playerScore++;
+        } else if (roundResult.includes('You lose!')) {
+            computerScore++;
+        }
+    }
+
+    console.log(`Final score: Player ${playerScore} - ${computerScore} Computer`)
+
+    if (playerScore > computerScore) {
+        console.log('Congratulations! You won the game.');
+    } else if (playerScore < computerScore) {
+        console.log('Sorry, you lost the game.');
+    } else {
+        console.log('The game was a tie.');
+    }
+}
+
+game();
+
+// const playerSelection = prompt('Let\'s play rock, paper scissors. Type your hand position below').toLowerCase();
+// const computerSelection = getComputerChoice(myArray);
